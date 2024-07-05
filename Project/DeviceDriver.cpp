3a6b79dac9695bd32a2d13681a5c54a71f1bb4ca
juheon.song <bootplus@naver.com>
2024-07-05 09:36:32 +0900

    [f] device read 5 count

--- Project/DeviceDriver.cpp
+++ Project/DeviceDriver.cpp
@@ -5,8 +5,11 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int rst = 0;
+    for (int cnt = 0; cnt < 5; ++cnt) {
+        rst = (int)(m_hardware->read(address));
+    }
+    return rst;
 }
 
 void DeviceDriver::write(long address, int data)
